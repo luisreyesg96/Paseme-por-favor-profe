cef8ea758bda1ca7f70a5e52c45114f3cd304b8e
luisreyesg96 <138535252+luisreyesg96@users.noreply.github.com>
2023-07-03 22:11:52 -0600

    Practica 6
    uso del tabulador y salto de linea

---
+++ Salto_linea_y_tab.py
@@ -0,0 +1,7 @@
+
+#Uso del tabulador y salto de linea
+
+print("Resultado:\n\t-Python.\n\t-Javascript.\n\t-Java.\n\t-PHP.\n\t-Typescript.\n\t-SQL.\n\t-COBOL.")
+
+#en este caso, se imprime todo dentro del mismo print
+#usando los saltos de linea y tabuladores
