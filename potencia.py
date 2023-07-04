e59f929f3e22a10464609d991cf7447f42701622
luisreyesg96 <138535252+luisreyesg96@users.noreply.github.com>
2023-07-03 22:39:03 -0600

    Practica 8
    uso del operador de potencia

---
+++ potencia.py
@@ -0,0 +1,10 @@
+#uso de exponentes
+
+#la idea es usar como base el 2, para poder imprimir 1024
+
+#lo que hice fue dividir 1024 entre 2 n veces, y esa es el exponente
+#a usar
+
+n = 2 ** 10
+
+print (n)
