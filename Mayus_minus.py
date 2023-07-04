122fc4787d7a2144175d1e165bd9b856d5b6ebaf
luisreyesg96 <138535252+luisreyesg96@users.noreply.github.com>
2023-07-03 21:59:21 -0600

    Practica5
    Uso de Upper y Lower

---
+++ Mayus_minus.py
@@ -0,0 +1,12 @@
+#definimos una palabra en string
+nombre = "Luis Jorge Reyes"
+print (nombre)
+#imprimimos tal cual ese string
+
+nombre = "Luis Jorge Reyes".lower()
+print (nombre)
+#ahora todo en minusculas
+
+nombre = "Luis Jorge Reyes".upper()
+print (nombre)
+#y ahora todo en mayusculas
