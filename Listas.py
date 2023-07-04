6a5755957756cdbe2858f3e2e9ed0cc98c2ca507
luisreyesg96 <138535252+luisreyesg96@users.noreply.github.com>
2023-07-03 22:59:04 -0600

    Practica 10
    Uso de listas

---
+++ Listas.py
@@ -0,0 +1,16 @@
+
+#uso de listas en python
+
+#primero, definimos la primer lista, e imprimimos el valor
+#que esta en la posicion 3
+
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+
+#imprimimos el valor en la posicion 3
+print (colores[3])
+
+#creamos una lista con los valores indicados en sus posiciones
+lista = ["tres","dos", "cinco", "cuatro", "uno"]
+
+#imprimimos la lista
+print (lista)
