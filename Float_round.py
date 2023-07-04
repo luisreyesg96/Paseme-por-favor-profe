e4289fc52f7afb87243c3be5c3d9130040d6337e
luisreyesg96 <138535252+luisreyesg96@users.noreply.github.com>
2023-07-03 22:52:19 -0600

    Practica 9
    Uso de variable float y método round

---
+++ Float_round.py
@@ -0,0 +1,10 @@
+
+#uso del metodo round
+
+#primero, le asignamos a una variable float el valor solicitado
+
+valor = 17.567383292929200234
+
+#para imprimir, solo es necesario utilizar el metodo round
+
+print (round(valor, 5))
