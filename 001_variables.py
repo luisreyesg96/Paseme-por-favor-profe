945e2912dac962fbd4cae65a648252f4fe30e7ad
luisreyesg96 <138535252+luisreyesg96@users.noreply.github.com>
2023-07-03 18:06:21 -0600

    Práctica 1
    Hola mundo, se definen variables

---
+++ 001_variables.py
@@ -0,0 +1,5 @@
+palabra = "Hola mundo" #Asignamos "Hola mundo" a variable palabra
+var1=1 ; var2=10 #se escribe de esta manera cuando ponemos dos lineas juntas
+print (palabra)
+var3 = var1 + var2 #sumamos ambas variables
+print (var3)
