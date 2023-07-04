5929f4676e0c09316873623765fe6b180b7c8215
luisreyesg96 <138535252+luisreyesg96@users.noreply.github.com>
2023-07-03 22:32:08 -0600

    Practica 7
    uso de los diferentes operadores matematicos basicos

---
+++ Operaciones_mat.py
@@ -0,0 +1,12 @@
+#Operaciones matematicas
+
+#uso de los diferentes operadores matemáticos basicos
+suma = 20 + 23 + 44
+resta = 20 - 23 - 84
+multiplicacion = 37*23+20-1
+division = (5000/230)/2
+
+operacion = (10+5-15)/17
+
+#imprimimos el resultado de las diferentes operaciones realizadas
+print (suma, resta, multiplicacion, division, operacion)
