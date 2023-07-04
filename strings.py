55b018bfe9a8861396dc0f457a6e7694b89315f6
luisreyesg96 <138535252+luisreyesg96@users.noreply.github.com>
2023-07-03 18:10:47 -0600

    Practica 2
    Se definen variables string y se imprimen

---
+++ strings.py
@@ -0,0 +1,9 @@
+#en este caso, se pueden usar comillas dentro de un texto
+#siempre y cuando se usen comillas simples
+#ademas de que los comentarios se declaran diferente
+
+mensaje1 = "Este es la parte '1'"
+mensaje2 = "Este es la parte '2'"
+
+print (mensaje1)
+print (mensaje2)
